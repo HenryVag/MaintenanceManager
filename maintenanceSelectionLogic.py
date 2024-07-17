f3e7a075d5969062959d163541d3c60765367986
HenryVag <henry.vag@edu.turkuamk.fi>
2024-07-17 19:00:36 +0300

    Maintenance selection calculates and prints upcoming maintenance mileage

--- maintenanceSelectionLogic.py
+++ maintenanceSelectionLogic.py
@@ -3,4 +3,12 @@
 
 
 def maintenance_selection(mileage):
+    mileage = float(mileage)
+    if mileage < 6000:
+        print("perform 1K maintenance according to manual")
+
+    else:
+        maintenance_variable = mileage/6000
+        upcoming_maintenance_mileage = int(maintenance_variable) * 6000 + 6000
     
+    print(upcoming_maintenance_mileage)
